f3feb7dfba2bb152c07e7d45a170a511b1c81670
gbhaskara <gbhaskara@handson.com>
2012-02-21 09:28:25 -0800

    move shardsconfig to lib

--- lib/afterburner.rb
+++ lib/afterburner.rb
@@ -1,5 +1,3 @@
-require "yaml"
-require "erb"
 require 'afterburner/version'
 
 module Afterburner
@@ -11,63 +9,10 @@ module Afterburner
     end
   end
 
-
-  class << self
-    attr_accessor :VBUCKET_COUNT
-    attr_accessor :SHARDS_CONFIG
-    attr_accessor :shards_config_parsed
-    attr_accessor :REF_VBUCKET
-  end
-
-  def self.directory()
-    @directory ||= defined?(Rails) ?  Rails.root.to_s : Dir.pwd
-  end
-
-  def self.read_config
-    unless self.shards_config_parsed
-      self.REF_VBUCKET = -1
-      self.SHARDS_CONFIG = {}
-      shard_yml = YAML::load(ERB.new(IO.read(Afterburner.directory() + "config/environments/#{::Rails.env}_shards.yml")).result)
-      # Virtual bucket
-      # Every sharded entry falls into exactly one bucket
-      # A given set of buckets in each environment is mapped to a database based on SHARDS_COUNT
-      # Why 720  ?
-      #   because it is the LCM of 1,2,3,4,6,8,10,12,16,20,24,30,40 => easy migration to SHARDS_COUNT
-      self.VBUCKET_COUNT = shard_yml['vbucket_count'].to_i || 720
-
-      ['db', 'redis'].each do |type|
-        self.SHARDS_CONFIG[type]={}
-        shard_yml[type].keys.each do |k|
-          if k != 'master'
-            self.SHARDS_CONFIG[type][k] = {}
-            self.SHARDS_CONFIG[type][k]['shards_count'] = shard_yml[type][k]['shards_count'].to_i
-            self.SHARDS_CONFIG[type][k]['shards_count'].times do |i|
-              self.SHARDS_CONFIG[type][k][i] = {}
-              self.SHARDS_CONFIG[type][k][i]['ip'] = shard_yml[type][k][i]['ip'] || 'localhost'
-              self.SHARDS_CONFIG[type][k][i]['port'] = shard_yml[type][k][i]['port'] || 3369
-              self.SHARDS_CONFIG[type][k][i]['username'] = shard_yml[type][k][i]['username'] || 'root'
-              self.SHARDS_CONFIG[type][k][i]['password'] = shard_yml[type][k][i]['password'] || ''
-            end
-          elsif k == 'master'
-            self.SHARDS_CONFIG[type][k] = {}
-            self.SHARDS_CONFIG[type][k]['ip'] = shard_yml[type][k]['ip'] || 'localhost'
-            self.SHARDS_CONFIG[type][k]['port'] = shard_yml[type][k]['port'] || 3369
-            self.SHARDS_CONFIG[type][k]['username'] = shard_yml[type][k]['username'] || 'root'
-            self.SHARDS_CONFIG[type][k]['password'] = shard_yml[type][k]['password'] || ''
-          end
-        end
-      end
-      Afterburner.shards_config_parsed = true
-    end
-  end
 end
 
 REF = -1
 
-# create global shards hash
-Afterburner.read_config()
-
-#require 'afterburner/railtie'
 require 'afterburner/sharded_model'
 require 'afterburner/sharded_associations'
 require 'afterburner/sharded_redis'
@@ -76,4 +21,3 @@ require 'afterburner/ar_extensions'
 #require 'afterburner/acts_as_locally_cached'
 #require 'afterburner/analytics'
 
-#Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
---
+++ lib/afterburner/shards_config.rb
@@ -0,0 +1,59 @@
+require "yaml"
+require "erb"
+
+module Afterburner
+
+  class << self
+    attr_accessor :VBUCKET_COUNT
+    attr_accessor :SHARDS_CONFIG
+    attr_accessor :shards_config_parsed
+    attr_accessor :REF_VBUCKET
+  end
+
+  def self.directory()
+    @directory ||= defined?(Rails) ?  Rails.root.to_s : Dir.pwd
+  end
+
+  def self.read_config
+    unless self.shards_config_parsed
+      self.REF_VBUCKET = -1
+      self.SHARDS_CONFIG = {}
+      shard_yml = YAML::load(ERB.new(IO.read(Afterburner.directory() + "config/environments/#{::Rails.env}_shards.yml")).result)
+      # Virtual bucket
+      # Every sharded entry falls into exactly one bucket
+      # A given set of buckets in each environment is mapped to a database based on SHARDS_COUNT
+      # Why 720  ?
+      #   because it is the LCM of 1,2,3,4,6,8,10,12,16,20,24,30,40 => easy migration to SHARDS_COUNT
+      self.VBUCKET_COUNT = shard_yml['vbucket_count'].to_i || 720
+
+      ['db', 'redis'].each do |type|
+        self.SHARDS_CONFIG[type]={}
+        shard_yml[type].keys.each do |k|
+          if k != 'master'
+            self.SHARDS_CONFIG[type][k] = {}
+            self.SHARDS_CONFIG[type][k]['shards_count'] = shard_yml[type][k]['shards_count'].to_i
+            self.SHARDS_CONFIG[type][k]['shards_count'].times do |i|
+              self.SHARDS_CONFIG[type][k][i] = {}
+              self.SHARDS_CONFIG[type][k][i]['ip'] = shard_yml[type][k][i]['ip'] || 'localhost'
+              self.SHARDS_CONFIG[type][k][i]['port'] = shard_yml[type][k][i]['port'] || 3369
+              self.SHARDS_CONFIG[type][k][i]['username'] = shard_yml[type][k][i]['username'] || 'root'
+              self.SHARDS_CONFIG[type][k][i]['password'] = shard_yml[type][k][i]['password'] || ''
+            end
+          elsif k == 'master'
+            self.SHARDS_CONFIG[type][k] = {}
+            self.SHARDS_CONFIG[type][k]['ip'] = shard_yml[type][k]['ip'] || 'localhost'
+            self.SHARDS_CONFIG[type][k]['port'] = shard_yml[type][k]['port'] || 3369
+            self.SHARDS_CONFIG[type][k]['username'] = shard_yml[type][k]['username'] || 'root'
+            self.SHARDS_CONFIG[type][k]['password'] = shard_yml[type][k]['password'] || ''
+          end
+        end
+      end
+      Afterburner.shards_config_parsed = true
+    end
+  end
+end
+
+REF = -1
+
+# create global shards hash
+#Afterburner.read_config()
